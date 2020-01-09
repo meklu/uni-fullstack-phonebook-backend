4c0527fb67e3a56f494ca9c9b5ada93ca13b6a72
Melker Narikka <melker.narikka@helsinki.fi>
2020-01-09 22:40:31 +0200

    uibuild

---
+++ build/precache-manifest.425836647e648a1d725fbcfa58b2caff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4eab1498a1741a1096719bc3693dbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89fb53e5da93eb8896ce",
+    "url": "/static/css/main.3250f8a4.chunk.css"
+  },
+  {
+    "revision": "8a34f1483ede493eec88",
+    "url": "/static/js/2.881fcb6a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.881fcb6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89fb53e5da93eb8896ce",
+    "url": "/static/js/main.053c65e6.chunk.js"
+  },
+  {
+    "revision": "4058619cfd054eca1abc",
+    "url": "/static/js/runtime-main.43089770.js"
+  }
+]);
