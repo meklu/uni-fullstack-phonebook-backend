3dbf1ea93338b42e02e1dc0412118aac5e5891b2
Melker Narikka <melker.narikka@helsinki.fi>
2020-01-09 22:01:45 +0200

    p3.8 done

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ let persons = [
 ]
 
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
+
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.get('/api/persons', (req, res) => {
 	res.json(persons)
