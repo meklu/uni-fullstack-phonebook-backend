e4715e9989328f0436e9b2f8fc09a41fc109fda4
Melker Narikka <melker.narikka@helsinki.fi>
2020-01-09 20:47:05 +0200

    p3.1 done

--- index.js
+++ index.js
@@ -1,3 +1,38 @@
-const express = require('express')
+const PORT = 3001
 
-console.log('yo, sup?')
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+let persons = [
+	{
+		"name": "Arto Hellas",
+		"number": "040-123456",
+		"id": 1
+	},
+	{
+		"name": "Ada Lovelace",
+		"number": "39-44-5323523",
+		"id": 2
+	},
+	{
+		"name": "Dan Abramov",
+		"number": "12-43-234345",
+		"id": 3
+	},
+	{
+		"name": "Mary Poppendieck",
+		"number": "39-23-6423122",
+		"id": 4
+	}
+]
+
+app.use(bodyParser.json())
+
+app.get('/api/persons', (req, resp) => {
+	resp.json(persons)
+})
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port ${PORT}`)
+})
